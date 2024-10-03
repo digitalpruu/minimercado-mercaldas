e3b77c9c23ad9885451ba56be76bcf09b4417144
kevinpicon01 <digitalpruu@gmail.com>
2024-10-02 21:55:39 -0500

    Fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -13,8 +13,9 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "En Minimercado Mercaldas, nos dedicamos a ofrecer una variedad de víveres, apoyando a productores y comerciantes locales.",
-        description2: "Nuestro compromiso es brindar productos frescos y de calidad, contribuyendo al desarrollo de la comunidad."
+        description1: "En Minimercado Mercaldas, nos dedicamos a ofrecer una amplia gama de víveres y productos esenciales para tu hogar. Nuestro objetivo es proporcionar una experiencia de compra conveniente y accesible, apoyando a productores y comerciantes locales en el proceso. Desde alimentos frescos hasta productos de uso diario, cada artículo en nuestro mercado es seleccionado con cuidado para garantizar la mejor calidad y\n" +
+            "frescura. Nos enorgullece ser una opción confiable para todas tus necesidades de víveres. En Mercaldas, creemos en el poder de apoyar a los productores y comerciantes locales, fortaleciendo la economía comunitaria y ofreciendo productos de excelente calidad. ¡Visitanos y descubre cómo nuestro minimercado puede ser parte de tu vida diaria!\n" +
+            "¡Te esperamos en Minimercado Mercaldas, donde cada compra apoya a la comunidad y satisface tus necesidades!"
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
